c63ef71172318ff5ffa7e6378ab441bb3f4b6168
Michael Patterson <mjpatter88@gmail.com>
2017-08-16 19:25:57 -0500

    Add a simple example of using ctype unions.

---
+++ GrayHatPython/unions.py
@@ -0,0 +1,17 @@
+## Something about this seems to blow up visual studios debug/run
+## But it works fine in the terminal
+from ctypes import *
+
+class barley_amount(Union):
+    _fields_ = [
+        ("barley_long", c_long),
+        ("barley_int", c_int),
+        ("barley_char", c_char * 8),
+    ]
+
+value = input("Enter the amount of barley to put into the beer:")
+my_barley = barley_amount(int(value))
+print("Barley amount as a long: {}".format(my_barley.barley_long))
+print("Barley amount as a int: {}".format(my_barley.barley_int))
+print("Barley amount as a char: {}".format(my_barley.barley_char))
+print(str(my_barley.barley_char))
