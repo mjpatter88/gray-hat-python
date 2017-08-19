968fb7e2cc8f764e873ffc2f103cc51f9244253e
Michael Patterson <mjpatter88@gmail.com>
2017-08-18 23:11:44 -0500

    Add linux version of calling the c runtime printf function.

--- GrayHatPython/printf.py
+++ GrayHatPython/printf.py
@@ -1,6 +1,11 @@
-
+import os
 from ctypes import *
 
-msvcrt = cdll.msvcrt
-
-msvcrt.printf(b"Hello from c!\n")
+if os.name == 'nt':
+    msvcrt = cdll.msvcrt
+    msvcrt.printf(b"Hello from the Windows c runtime!\n")
+elif os.name == 'posix':
+    libc = CDLL("libc.so.6")
+    libc.printf(b"Hello from the Linux c runtime!\n")
+else:
+    print("Unsupported OS")
